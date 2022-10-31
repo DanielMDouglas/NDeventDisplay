341bc6f9c494c15a37deac10baa1e89c7c0f9de4
Daniel Douglas <dougl215@msu.edu>
2022-10-31 12:03:51 -0700

    updated dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ VER = "0.0.1"
 reqs = ["numpy",
         "h5py",
         "matplotlib>=3.6",
-        "LarpixParser @ git+https://github.com/DanielMDouglas/larpix_readout_parser.git",
+        "LarpixParser @ git+https://github.com/YifanC/larpix_readout_parser@develop",
         "tk"]
 
 setuptools.setup(
